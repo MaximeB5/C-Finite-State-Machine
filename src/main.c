3fe789d89da0241e1401a379c9efd306021fbf6b
MaximeB5 <belavalmax@sfr.fr>
2021-01-28 10:30:11 +0100

    fsm is working

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 // My Includes
-	// None for the moment.
+#include "../inc/fsm.h"
 
 // Includes
 #include <stdio.h>
@@ -10,10 +10,41 @@
 
 int main()
 {
-	printf("Hello from C-Finite State Machine.\n\n");
+	printf("Hello from C-Finite State Machine.\n");
+	printf("This FSM follows a sequence of 5 states, from 0 to 4.\n\n");
 
-	// ---------------------------------------------------------------------------------
-	// ---------------------------------------------------------------------------------
+	// Initialize array of structure with states and event with proper handler
+	s_FSM fsm [] =
+	{
+		{Idle_State,			Card_Insert_Event,		insert_card_handler},
+		{Card_Inserted_State,	Pin_Enter_Event,		enter_pin_handler},
+		{Pin_Eentered_State,	Option_Selection_Event,	option_selection_handler},
+		{Option_Selected_State,	Amount_Enter_Event,		enter_amount_handler},
+		{Amount_Entered_State,	Amount_Dispatch_Event,	amount_dispatch_handler}
+	};
+
+	eSystemState next_state = Idle_State;
+
+	while(1)
+    {
+        // Get event
+        eSystemEvent eNewEvent = read_event();
+		printf("State is %d\n", eNewEvent);
+
+        if(		(next_state < last_State)
+			&&	(eNewEvent < last_Event)
+			&&	(fsm[next_state].event == eNewEvent)
+			&&	(fsm[next_state].event_handler != NULL)
+		)
+        {
+            // Function call as per the state and event and return the next state of the finite state machine
+            next_state = (*fsm[next_state].event_handler)();
+        }
+        else
+        {
+			printf("--- invalid input ---\n\n");
+        }
+    }
 
 	return(0);
 }
